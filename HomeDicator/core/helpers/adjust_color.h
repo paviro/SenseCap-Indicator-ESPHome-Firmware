030120be9143e18d161f3e46870f2ce59d77fba0
Paul-Vincent Roll <paviro@me.com>
2024-12-24 15:53:06 +0100

    Removed whitespaces

--- HomeDicator/core/helpers/adjust_color.h
+++ HomeDicator/core/helpers/adjust_color.h
@@ -3,20 +3,20 @@ lv_color_t adjust_color(uint32_t color, float darken_factor, float saturation_re
 	uint8_t red = (color >> 16) & 0xFF;
 	uint8_t green = (color >> 8) & 0xFF;
 	uint8_t blue = color & 0xFF;
-	
+
 	// Darken components
 	red = static_cast<uint8_t>(red * darken_factor);
 	green = static_cast<uint8_t>(green * darken_factor);
 	blue = static_cast<uint8_t>(blue * darken_factor);
-	
+
 	// Convert to grayscale (simple average)
 	uint8_t grayscale = static_cast<uint8_t>((red + green + blue) / 3);
-	
+
 	// Apply the saturation reduction by blending the grayscale value with the original color
 	red = static_cast<uint8_t>(red * (1 - saturation_reduction_factor) + grayscale * saturation_reduction_factor);
 	green = static_cast<uint8_t>(green * (1 - saturation_reduction_factor) + grayscale * saturation_reduction_factor);
 	blue = static_cast<uint8_t>(blue * (1 - saturation_reduction_factor) + grayscale * saturation_reduction_factor);
-	
+
 	// Return the adjusted color using lv_color_make with the desaturated, darkened components
 	return lv_color_make(red, green, blue);
 }
--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-This is just the ESPHome package part, for the main configuration and info on how to use it go [here](https://github.com/HomeDicator).  
+This is just the ESPHome package part, for the main configuration and info on how to use it go [here](https://github.com/HomeDicator).
 
 We will be able to fetch this package automatically once [this](https://github.com/esphome/esphome/pull/7606) is added to ESPHome.
 
 ## Development
-You can run a version of this firmware using SDL on your development machine using the `HomeDicator.yaml` file.  
+You can run a version of this firmware using SDL on your development machine using the `HomeDicator.yaml` file.
 
 1. Follow [these](https://esphome.io/guides/installing_esphome.html) instructions to install ESPHome on your computer.
 2. Install the requirements listed [here](https://esphome.io/components/display/sdl.html).
